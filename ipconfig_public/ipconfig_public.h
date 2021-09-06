c2629498a342aa614bda2a8c29c674efa0908ee4
CASL0 <gin.0046.822@gmail.com>
2021-09-06 09:34:35 +0900

    initial commit

---
+++ ipconfig_public/ipconfig_public.h
@@ -0,0 +1,32 @@
+﻿
+// ipconfig_public.h : PROJECT_NAME アプリケーションのメイン ヘッダー ファイルです
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'pch.h' をインクルードしてください"
+#endif
+
+#include "Resource.h"		// メイン シンボル
+
+
+// CipconfigpublicApp:
+// このクラスの実装については、ipconfig_public.cpp を参照してください
+//
+
+class CipconfigpublicApp : public CWinApp
+{
+public:
+	CipconfigpublicApp();
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+
+// 実装
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CipconfigpublicApp theApp;
---
+++ ipconfig_public/ipconfig_publicDlg.h
@@ -0,0 +1,33 @@
+﻿
+// ipconfig_publicDlg.h : ヘッダー ファイル
+//
+
+#pragma once
+
+
+// CipconfigpublicDlg ダイアログ
+class CipconfigpublicDlg : public CDialogEx
+{
+// コンストラクション
+public:
+	CipconfigpublicDlg(CWnd* pParent = nullptr);	// 標準コンストラクター
+
+// ダイアログ データ
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_IPCONFIG_PUBLIC_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV サポート
+
+
+// 実装
+protected:
+	HICON m_hIcon;
+
+	// 生成された、メッセージ割り当て関数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
